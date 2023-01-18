58b86671d972a3b4a3134400f96c5b33807ee637
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2023-01-18 20:01:01 +0300

    GenericInputDtoAdded

---
+++ Core/Dtos/GenericDto/GenericInputDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos.GenericDto
+{
+     public class GenericInputDto<T>
+    {
+        public T? Data { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+        //public string? OrderedColumn { get; set; }
+        //public string? OrderType { get; set; }
+
+
+        public static GenericInputDto<T> ResponseData(T? data, int skip = 0, int take = int.MaxValue, /*string orderedColumn = "Id", string orderType = EntityOrderType.OrderAsc*/)
+        {
+            return new GenericInputDto<T> { Data = data, Skip = skip, Take = take/*, OrderedColumn = orderedColumn, OrderType = orderType*/ };
+        }
+
+
+
+    }
+}
---
+++ Core/Dtos/GenericDto/GenericResponseDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos.GenericDto
+{
+    public class GenericResponseDto<TEntity> 
+    {
+
+        public GenericResponseDto()
+        {
+
+        }
+
+        public TEntity? Data { get; set; }
+
+        public bool IsSuccessful { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public string? Error { get; set; }
+        public int? TotalCount { get; set; }
+
+        public static GenericResponseDto<TEntity> ResponseData(TEntity? data, int statusCode, string? error, int? totalCount = 0)
+        {
+            return new GenericResponseDto<TEntity> { Data = data, StatusCode = statusCode, IsSuccessful = !String.IsNullOrEmpty(error) ? false : true, Error = error, TotalCount = totalCount };
+        }
+
+    }
+}
