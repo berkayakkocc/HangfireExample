8c3b29e3a8ad75e717328afe60ab9243524953f0
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2023-01-18 21:00:10 +0300

    Fixed

--- Core/Dtos/GenericDto/GenericInputDto.cs
+++ Core/Dtos/GenericDto/GenericInputDto.cs
@@ -15,7 +15,7 @@ namespace Core.Dtos.GenericDto
         //public string? OrderType { get; set; }
 
 
-        public static GenericInputDto<T> ResponseData(T? data, int skip = 0, int take = int.MaxValue, /*string orderedColumn = "Id", string orderType = EntityOrderType.OrderAsc*/)
+        public static GenericInputDto<T> ResponseData(T? data, int skip = 0, int take = int.MaxValue /*string orderedColumn = "Id", string orderType = EntityOrderType.OrderAsc*/)
         {
             return new GenericInputDto<T> { Data = data, Skip = skip, Take = take/*, OrderedColumn = orderedColumn, OrderType = orderType*/ };
         }
